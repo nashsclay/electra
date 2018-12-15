82afb92acd1e96abbec6f8cb2afc61b0fb71baef
John <ComputerCraftr@users.noreply.github.com>
2018-12-15 05:08:30 -0700

    Update more strings

--- src/qt/blockexplorer.cpp
+++ src/qt/blockexplorer.cpp
@@ -479,7 +479,7 @@ void BlockExplorer::showEvent(QShowEvent*)
 
         if (!GetBoolArg("-txindex", true)) {
             QString Warning = tr("Not all transactions will be shown. To view all transactions you need to set txindex=1 in the configuration file (myce.conf).");
-            QMessageBox::warning(this, "MYCE Core Blockchain Explorer", Warning, QMessageBox::Ok);
+            QMessageBox::warning(this, "Electra Core Blockchain Explorer", Warning, QMessageBox::Ok);
         }
     }
 }
